fd8f6a477caf70ad2c20ffa5d20ac11016d85983
shaddollxz <shaddollxz@163.com>
2023-07-18 16:50:33 +0800

    fix: eslint-vue parser change

---
+++ .changeset/itchy-laws-confess.md
@@ -0,0 +1,6 @@
+---
+'@byzanteam/eslint-config-vue-ts': patch
+'@byzanteam/eslint-config-ts': patch
+---
+
+add type-checked rules and fix type-checked error when use vue
--- packages/eslint-ts/index.js
+++ packages/eslint-ts/index.js
@@ -8,6 +8,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:import/errors',
     'plugin:prettier/recommended',
   ],
