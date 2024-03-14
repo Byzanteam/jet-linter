119d8ae0e6f4de898fe990b51a41627ef00055d9
shaddollxz <shaddollxz@163.com>
2024-03-14 15:14:57 +0800

    feat: import lint allow typescript

---
+++ .changeset/large-shrimps-kneel.md
@@ -0,0 +1,7 @@
+---
+"@byzanteam/eslint-config-react-ts": patch
+"@byzanteam/eslint-config-vue-ts": patch
+"@byzanteam/eslint-config-ts": patch
+---
+
+import lint check use eslint-import-resolver-typescript
--- packages/eslint-vue-ts/index.js
+++ packages/eslint-vue-ts/index.js
@@ -24,12 +24,4 @@ module.exports = {
     'vue/attribute-hyphenation': 'error',
     'vue/no-setup-props-destructure': 'warn',
   },
-  settings: {
-    'import/resolver': {
-      alias: {
-        map: [['@', './src']],
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
-      },
-    },
-  },
 }
